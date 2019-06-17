c93aba9fdee714ff6643dda50e89e148b361f8d7
devServer <itsupport@agusto.com>
2019-06-17 14:08:50 +0100

    . . .

--- src/updater.js
+++ src/updater.js
@@ -4,5 +4,11 @@ autoUpdater.logger.transports.file.level = "info";
 
 exports.check = () => {
     // Start Update Check . . . .
+    autoUpdater.setFeedURL({
+        provider: "github",
+        owner: "AgustoDev",
+        repo: "AStore_App",
+        token: "b387d433f67af7da5d28f26a0a68f0f4c9a2e9d6"
+    });
     autoUpdater.checkForUpdates();
 };
